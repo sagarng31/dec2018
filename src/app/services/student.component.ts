51348eb14f255f429465881ff5351b2bcd3ff078
Sagar Patil <sagar.cuelogic@nextseed.com>
2018-12-11 09:19:56 +0530

    consume service

--- src/app/services/student.component.ts
+++ src/app/services/student.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { studentService } from './student.sevice';
 
 @Component({
     selector:'stud-comp-serv',
-    templateUrl:'./student.component.html'
+    templateUrl:'./student.component.html',
+    providers:[studentService]
 })
 
 export class StudentComponentServ{
     public title:string = 'Service Demo';
+
+    public studentInfo;
+
+    constructor(private st: studentService){
+
+    }
+
+    public showStudent(){
+        this.studentInfo = this.st.getStudent();
+    }
 }
